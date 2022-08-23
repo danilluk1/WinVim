1436b2eb4e5d6abf10ee37b44becac75a6a52a9b
danluki02 <danluki02@yandex.ru>
2022-08-23 19:34:35 +0500

    Добавьте файлы проекта.

---
+++ WinVim/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Program {
+    private delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
+    [DllImport("User32.dll")]
+    static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+    static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+    [DllImport("User32.dll")]
+    static extern short GetAsyncKeyState();
+    public static void Main(string[] args) {
+        MessageBox(IntPtr.Zero, "Command line message box", "Attention!", 0);
+    }
+}
---
+++ WinWin.BL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WinWin.BL
+{
+    public class Class1
+    {
+
+    }
+}
