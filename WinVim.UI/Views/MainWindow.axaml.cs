b3def85924b826bc6af953d74de4ed7a5f51fd31
danluki02 <danluki02@yandex.ru>
2022-09-08 16:20:39 +0500

    settings window starting position

--- WinVim.UI/Views/MainWindow.axaml.cs
+++ WinVim.UI/Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -8,14 +9,42 @@ namespace WinVim.UI
 {
     public partial class MainWindow : Window
     {
+        private bool isSettingsShown = false;
+        private SettingsWindow window;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public void OnSettingsClick(object sender, RoutedEventArgs e) {
-            SettingsWindow window = new SettingsWindow();
+        public void OnFocusLost(object sender, RoutedEventArgs e) {
+            window?.Close();
+        }
+
+        public void OnSettingsEnter(object sender, PointerEventArgs e) {
+            if (isSettingsShown) {
+                window.Close();
+                isSettingsShown = false;
+                return;
+            }
+
+            var curScreen = Screens.ScreenFromPoint(this.Position);
+            if (curScreen == null) return;
+
+            PixelPoint settingsWindowPoint;
+            if(this.Position.Y < (curScreen.Bounds.Height / 2)) {
+                settingsWindowPoint = new PixelPoint(this.Position.X, this.Position.Y + 40);
+            } else {
+                settingsWindowPoint = new PixelPoint(this.Position.X, this.Position.Y - 85);
+            }
+
+            window = new SettingsWindow();
+            window.Position = settingsWindowPoint;
             window.Show();
+            isSettingsShown = true;
+        }
+
+        public void OnFocus(object sender, PointerEventArgs e) {
+            //throw new NotImplementedException();
         }
     }
 }
--- WinVim.UI/Views/SettingsWindow.axaml.cs
+++ WinVim.UI/Views/SettingsWindow.axaml.cs
@@ -1,9 +1,16 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using System;
 
 namespace WinVim.UI.Views {
     public partial class SettingsWindow : Window {
         public SettingsWindow() {
             InitializeComponent();
+        }
+
+        public void OnFocusLost(object? sender, EventArgs e) {
+            this.Close();
         }
     }
 }
